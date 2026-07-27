f309567894d02dc19748c52220f1ae3513df8b0d
杨树 <ksyangshu@outlook.com>
2026-07-27 21:39:07 +0800

    fix: BAT改GBK(ANSI)无BOM编码, 删chcp65001, 根治BOM残留首行(@echo报错)与UTF-8乱码吞命令的闪退

--- install.bat
+++ install.bat
@@ -1,33 +1,32 @@
-﻿@echo off
-chcp 65001 >nul 2>&1
+@echo off
 setlocal EnableExtensions EnableDelayedExpansion
 
-:: 全锟斤拷锟斤拷志锟斤拷锟斤拷使锟斤拷锟斤拷也锟斤拷锟阶ｏ拷
+:: ȫ����־����ʹ����Ҳ���ף�
 set "LOG=%TEMP%\FileUnlocker_install.log"
-echo [%date% %time%] 锟斤拷锟斤拷 install.bat > "%LOG%"
-echo [%date% %time%] 锟斤拷前目录: %CD% >> "%LOG%"
-echo [%date% %time%] 锟脚憋拷路锟斤拷: %~f0 >> "%LOG%"
+echo [%date% %time%] ���� install.bat > "%LOG%"
+echo [%date% %time%] ��ǰĿ¼: %CD% >> "%LOG%"
+echo [%date% %time%] �ű�·��: %~f0 >> "%LOG%"
 
-:: ============ 锟斤拷锟斤拷员锟斤拷锟斤拷权 ============
+:: ============ ����Ա����Ȩ ============
 fltmc >nul 2>&1
 if errorlevel 1 (
-    echo [%date% %time%] 锟角癸拷锟斤拷员锟斤拷锟斤拷锟斤拷锟斤拷权 >> "%LOG%"
+    echo [%date% %time%] �ǹ���Ա��������Ȩ >> "%LOG%"
     powershell -NoProfile -Command "Start-Process -FilePath '%~f0' -Verb RunAs" >> "%LOG%" 2>&1
-    echo [%date% %time%] 锟窖凤拷锟斤拷锟斤拷权锟斤拷锟剿筹拷原锟斤拷锟斤拷 >> "%LOG%"
+    echo [%date% %time%] �ѷ�����Ȩ���˳�ԭ���� >> "%LOG%"
     exit /b
 )
-echo [%date% %time%] 锟斤拷锟角癸拷锟斤拷员 >> "%LOG%"
+echo [%date% %time%] ���ǹ���Ա >> "%LOG%"
 
-:: ============ 锟斤拷锟斤拷 ============
+:: ============ ���� ============
 set "INSTALL_DIR=C:\Program Files\FileUnlocker"
 set "REPO_URL=https://github.com/ksyangshu/FileUnlocker"
 set "PWSH_URL=https://github.com/PowerShell/PowerShell/releases/download/v7.5.0/PowerShell-7.5.0-win-x64.msi"
 set "HANDLE_URL1=https://download.sysinternals.com/files/Handle.zip"
 set "HANDLE_URL2=https://mirror.ghproxy.com/https://download.sysinternals.com/files/Handle.zip"
 
-echo [FileUnlocker 锟斤拷装] 锟斤拷始 > "%LOG%"
+echo [FileUnlocker ��װ] ��ʼ > "%LOG%"
 
-:: ============ 锟斤拷锟� PowerShell 7 ============
+:: ============ ���? PowerShell 7 ============
 set "PWSH_EXE="
 for %%P in ("%ProgramFiles%\PowerShell\7\pwsh.exe" "%ProgramFiles(x86)%\PowerShell\7\pwsh.exe") do (
     if exist %%~P set "PWSH_EXE=%%~P"
@@ -38,40 +37,40 @@ if not errorlevel 1 (
 )
 
 if not defined PWSH_EXE (
-    echo [锟斤拷锟絔 未锟斤拷锟斤拷 PowerShell 7 >> "%LOG%"
-    call :ask "未锟斤拷獾� PowerShell 7 (pwsh.exe)锟斤拷锟斤拷锟角憋拷锟斤拷锟竭的憋拷锟斤拷锟斤拷锟斤拷锟斤拷`n锟角凤拷锟斤拷锟斤拷锟斤拷锟截诧拷锟斤拷装锟斤拷`n(取锟斤拷锟斤拷锟剿筹拷锟斤拷装)" "锟斤拷装确锟斤拷"
+    echo [���] δ���� PowerShell 7 >> "%LOG%"
+    call :ask "δ���? PowerShell 7 (pwsh.exe)�����Ǳ����ߵı���������`n�Ƿ��������ز���װ��`n(ȡ�����˳���װ)" "��װȷ��"
     if errorlevel 1 (
-        echo [取锟斤拷] 锟矫伙拷锟杰撅拷锟斤拷装 PowerShell 7锟斤拷锟剿筹拷锟斤拷
+        echo [ȡ��] �û��ܾ���װ PowerShell 7���˳���
         goto :end_cancel
     )
     call :install_pwsh
     if errorlevel 1 goto :end_fail
-    :: 锟斤拷锟铰讹拷位
+    :: ���¶�λ
     for %%P in ("%ProgramFiles%\PowerShell\7\pwsh.exe") do set "PWSH_EXE=%%~P"
     if not exist "!PWSH_EXE!" (
         for /f "delims=" %%i in ('where pwsh.exe 2^>nul') do set "PWSH_EXE=%%i"
     )
 )
-echo [锟斤拷锟絔 使锟斤拷 PowerShell: %PWSH_EXE% >> "%LOG%"
+echo [���] ʹ�� PowerShell: %PWSH_EXE% >> "%LOG%"
 
-:: ============ 目录锟窖达拷锟斤拷锟斤拷询锟斤拷 ============
+:: ============ Ŀ¼�Ѵ�����ѯ�� ============
 if exist "%INSTALL_DIR%" (
-    call :ask "锟斤拷獾斤拷锟阶澳柯硷拷汛锟斤拷冢锟絗n%INSTALL_DIR%`n锟角否覆盖诧拷锟斤拷锟斤拷锟斤拷装锟斤拷" "锟斤拷装确锟斤拷"
+    call :ask "��⵽��װĿ¼�Ѵ��ڣ�`n%INSTALL_DIR%`n�Ƿ񸲸ǲ�������װ��" "��װȷ��"
     if errorlevel 1 (
-        echo [取锟斤拷] 锟矫伙拷取锟斤拷锟斤拷锟角★拷
+        echo [ȡ��] �û�ȡ�����ǡ�
         goto :end_cancel
     )
 )
 
-:: ============ 锟斤拷锟斤拷锟侥硷拷 ============
-echo [1/4] 锟斤拷锟斤拷锟侥硷拷锟斤拷 %INSTALL_DIR%
+:: ============ �����ļ� ============
+echo [1/4] �����ļ��� %INSTALL_DIR%
 if not exist "%INSTALL_DIR%" mkdir "%INSTALL_DIR%"
 xcopy "%~dp0src\*" "%INSTALL_DIR%\" /E /Y /Q >> "%LOG%" 2>&1
 set "VBS=%INSTALL_DIR%\FileUnlocker_Run.vbs"
 set "HANDLE_EXE=%INSTALL_DIR%\handle.exe"
 
 :: ============ handle.exe ============
-echo [2/4] 准锟斤拷 handle.exe
+echo [2/4] ׼�� handle.exe
 set "HANDLE_OK=0"
 if exist "%HANDLE_EXE%" (
     set "HANDLE_OK=1"
@@ -81,41 +80,41 @@ if "%HANDLE_OK%"=="0" (
     if errorlevel 1 goto :end_fail
 )
 
-:: ============ 注锟斤拷锟� ============
-echo [3/4] 注锟斤拷锟揭硷拷锟剿碉拷
+:: ============ ע���? ============
+echo [3/4] ע���Ҽ��˵�
 set "CMD=wscript.exe \"%VBS%\" \"%%1\""
 for %%S in (* AllFilesystemObjects Directory) do (
     set "KEY=HKLM\Software\Classes\%%S\shell\FileUnlocker"
-    reg add "!KEY!" /ve /t REG_SZ /d "锟斤拷锟斤拷募锟秸硷拷锟�" /f >nul 2>&1
+    reg add "!KEY!" /ve /t REG_SZ /d "����ļ�ռ��?" /f >nul 2>&1
     reg add "!KEY!" /v Icon /t REG_SZ /d "shell32.dll,131" /f >nul 2>&1
     reg add "!KEY!\command" /ve /t REG_SZ /d "%CMD%" /f >nul 2>&1
-    echo   锟斤拷注锟斤拷 %%S
+    echo   ��ע�� %%S
 )
-:: 锟斤拷锟斤拷锟斤拷 HKCU 锟斤拷锟斤拷
+:: ������ HKCU ����
 reg delete "HKCU\Software\Classes\*\shell\FileUnlocker" /f >nul 2>&1
 reg delete "HKCU\Software\Classes\Directory\shell\FileUnlocker" /f >nul 2>&1
 
-:: ============ 锟斤拷锟� ============
-echo [4/4] 锟斤拷锟�
-echo 锟斤拷锟斤拷锟斤拷源锟斤拷锟斤拷锟斤拷使锟揭硷拷锟剿碉拷锟斤拷锟斤拷锟斤拷效...
+:: ============ ���? ============
+echo [4/4] ���?
+echo ������Դ������ʹ�Ҽ��˵�������Ч...
 taskkill /IM explorer.exe /F >nul 2>&1
 start "" explorer.exe
 echo.
-echo ========== 锟斤拷锟斤拷锟斤拷锟斤拷 ==========
-echo 锟斤拷锟斤拷锟斤拷锟斤拷强锟斤拷锟斤拷止锟斤拷锟教凤拷式锟斤拷锟斤拷募锟�/锟侥硷拷锟斤拷占锟矫ｏ拷锟斤拷锟杰碉拷锟斤拷未锟斤拷锟斤拷锟斤拷锟捷讹拷失锟斤拷锟斤拷锟斤拷斐ｏ拷顺锟斤拷锟�
-echo 使锟斤拷锟斤拷锟斤拷锟斤拷锟叫承碉拷锟缴此诧拷锟斤拷锟斤拷锟轿何猴拷锟斤拷锟斤拷锟斤拷卟锟斤拷械锟斤拷魏锟街憋拷踊锟斤拷锟斤拷锟斤拷巍锟�
-echo 锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷止系统锟截硷拷锟斤拷锟教ｏ拷lsass锟斤拷svchost 锟饺ｏ拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟矫憋拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷艿锟斤拷锟较低筹拷锟斤拷榷锟斤拷锟�
-echo handle.exe 锟斤拷 Sysinternals(微锟斤拷) 锟结供锟斤拷锟斤拷锟斤拷锟竭斤拷锟斤拷使锟斤拷时锟斤拷锟斤拷俜锟皆达拷锟斤拷兀锟斤拷挚獠伙拷锟斤拷锟矫讹拷锟斤拷锟狡★拷
+echo ========== �������� ==========
+echo ��������ǿ����ֹ���̷�ʽ����ļ�?/�ļ���ռ�ã����ܵ���δ�������ݶ�ʧ������쳣�˳���?
+echo ʹ���������ге��ɴ˲������κκ�������߲��е��κ�ֱ�ӻ������Ρ�?
+echo ����������ֹϵͳ�ؼ����̣�lsass��svchost �ȣ������������ñ�������������ܵ���ϵͳ���ȶ���?
+echo handle.exe �� Sysinternals(΢��) �ṩ�������߽���ʹ��ʱ����ٷ�Դ���أ��ֿⲻ����ö����ơ�
 echo ==============================
 echo.
-echo 锟斤拷锟斤拷锟揭硷拷锟斤拷锟斤拷募锟斤拷锟斤拷募锟斤拷屑锟斤拷煽锟斤拷锟斤拷锟斤拷锟斤拷锟侥硷拷占锟矫★拷锟斤拷
-echo 锟斤拷装锟斤拷志: %LOG%
+echo �����Ҽ�����ļ����ļ��м��ɿ���������ļ�ռ�á���
+echo ��װ��־: %LOG%
 pause
 exit /b 0
 
-:: ============ 锟斤拷锟斤拷 ============
+:: ============ ���� ============
 :ask
-:: %1=锟斤拷息 %2=锟斤拷锟斤拷  锟斤拷锟斤拷 errorlevel 1=锟斤拷/取锟斤拷 0=锟斤拷
+:: %1=��Ϣ %2=����  ���� errorlevel 1=��/ȡ�� 0=��
 set "MSG=%~1"
 set "TITLE=%~2"
 set "VBS_TMP=%TEMP%\fu_ask.vbs"
@@ -133,16 +132,16 @@ del /f /q "%VBS_TMP%" 2>nul
 exit /b %RC%
 
 :install_pwsh
-echo [锟斤拷锟斤拷] PowerShell 7 ...
+echo [����] PowerShell 7 ...
 set "MSI=%TEMP%\PowerShell-7.msi"
 curl -L --max-time 120 -o "%MSI%" "%PWSH_URL%" 2>>"%LOG%"
 if not exist "%MSI%" (
-    echo [失锟斤拷] PowerShell 7 锟斤拷锟斤拷失锟杰ｏ拷锟斤拷锟今不可达）锟斤拷锟斤拷锟街讹拷锟斤拷装锟斤拷锟斤拷锟皆★拷
-    echo 锟劫凤拷: https://github.com/PowerShell/PowerShell/releases
+    echo [ʧ��] PowerShell 7 ����ʧ�ܣ����񲻿ɴ�����ֶ���װ�����ԡ�
+    echo �ٷ�: https://github.com/PowerShell/PowerShell/releases
     pause
     exit /b 1
 )
-echo [锟斤拷装] 锟斤拷默锟斤拷装 PowerShell 7 ...
+echo [��װ] ��Ĭ��װ PowerShell 7 ...
 msiexec /i "%MSI%" /quiet /norestart >> "%LOG%" 2>&1
 del /f /q "%MSI%" 2>nul
 exit /b 0
@@ -150,7 +149,7 @@ exit /b 0
 :download_handle
 set "ZIP=%TEMP%\Handle.zip"
 for %%U in ("%HANDLE_URL1%" "%HANDLE_URL2%") do (
-    echo   锟斤拷锟斤拷: %%U
+    echo   ����: %%U
     curl -L --max-time 90 -o "%ZIP%" "%%U" 2>>"%LOG%"
     if exist "%ZIP%" (
         powershell -NoProfile -Command "Expand-Archive -Path '%ZIP%' -DestinationPath '%TEMP%\handle_tmp' -Force" >> "%LOG%" 2>&1
@@ -160,21 +159,21 @@ for %%U in ("%HANDLE_URL1%" "%HANDLE_URL2%") do (
         del /f /q "%ZIP%" 2>nul
         rmdir /s /q "%TEMP%\handle_tmp" 2>nul
         if exist "%HANDLE_EXE%" (
-            echo   锟斤拷锟�
+            echo   ���?
             exit /b 0
         )
     )
 )
-echo [失锟斤拷] handle.exe 锟斤拷锟斤拷失锟杰ｏ拷锟斤拷锟街讹拷锟斤拷锟截放碉拷 %HANDLE_EXE%
+echo [ʧ��] handle.exe ����ʧ�ܣ����ֶ����طŵ� %HANDLE_EXE%
 pause
 exit /b 1
 
 :end_cancel
-echo 锟斤拷装锟斤拷取锟斤拷锟斤拷
+echo ��װ��ȡ����
 pause
 exit /b 0
 
 :end_fail
-echo 锟斤拷装失锟杰ｏ拷锟斤拷锟斤拷锟街� %LOG%
+echo ��װʧ�ܣ�������? %LOG%
 pause
 exit /b 1
--- uninstall.bat
+++ uninstall.bat
@@ -1,22 +1,22 @@
-﻿@echo off
+@echo off
 setlocal EnableExtensions
 
 set "INSTALL_DIR=C:\Program Files\FileUnlocker"
 set "LOG=%TEMP%\FileUnlocker_uninstall.log"
-echo [%date% %time%] 锟斤拷锟斤拷 uninstall.bat > "%LOG%"
+echo [%date% %time%] ���� uninstall.bat > "%LOG%"
 
-:: 锟斤拷锟斤拷员锟斤拷锟斤拷权
+:: ����Ա����Ȩ
 fltmc >nul 2>&1
 if errorlevel 1 (
-    echo [%date% %time%] 锟角癸拷锟斤拷员锟斤拷锟斤拷锟斤拷锟斤拷权 >> "%LOG%"
+    echo [%date% %time%] �ǹ���Ա��������Ȩ >> "%LOG%"
     powershell -NoProfile -Command "Start-Process -FilePath '%~f0' -Verb RunAs" >> "%LOG%" 2>&1
     exit /b
 )
-echo [%date% %time%] 锟斤拷锟角癸拷锟斤拷员 >> "%LOG%"
+echo [%date% %time%] ���ǹ���Ա >> "%LOG%"
 
-echo [FileUnlocker 卸锟斤拷] 锟斤拷始 > "%LOG%"
+echo [FileUnlocker ж��] ��ʼ > "%LOG%"
 
-:: 确锟较ｏ拷VBScript 锟斤拷锟斤拷锟斤拷锟斤拷锟斤拷 powershell 转锟斤拷锟斤拷锟解）
+:: ȷ�ϣ�VBScript ���������� powershell ת�����⣩
 set "VBS_TMP=%TEMP%\fu_ask_uninstall.vbs"
 (
     echo result = MsgBox(WScript.Arguments(0^), vbYesNo + vbQuestion, WScript.Arguments(1^)^)
@@ -26,36 +26,36 @@ set "VBS_TMP=%TEMP%\fu_ask_uninstall.vbs"
     echo     WScript.Quit 1
     echo End If
 ) > "%VBS_TMP%"
-cscript //NoLogo "%VBS_TMP%" "确锟斤拷要卸锟斤拷 FileUnlocker 锟斤拷删锟斤拷锟揭硷拷锟剿碉拷锟斤拷" "卸锟斤拷确锟斤拷"
+cscript //NoLogo "%VBS_TMP%" "ȷ��Ҫж�� FileUnlocker ��ɾ���Ҽ��˵���" "ж��ȷ��"
 if errorlevel 1 (
     del /f /q "%VBS_TMP%" 2>nul
-    echo 锟斤拷取锟斤拷卸锟截★拷
+    echo ��ȡ��ж�ء�
     pause
     exit /b 0
 )
 del /f /q "%VBS_TMP%" 2>nul
 
-:: 删注锟斤拷锟�
-echo 锟斤拷锟斤拷注锟斤拷锟斤拷锟�
+:: ɾע���?
+echo ����ע�����?
 for %%S in (* AllFilesystemObjects Directory) do (
     reg delete "HKLM\Software\Classes\%%S\shell\FileUnlocker" /f >nul 2>&1
-    echo   锟斤拷删锟斤拷 %%S
+    echo   ��ɾ�� %%S
 )
 reg delete "HKCU\Software\Classes\*\shell\FileUnlocker" /f >nul 2>&1
 reg delete "HKCU\Software\Classes\Directory\shell\FileUnlocker" /f >nul 2>&1
 
-:: 注锟斤拷锟狡伙拷锟斤拷锟斤拷
+:: ע���ƻ�����
 schtasks /Delete /TN "WinDiag_Unlock_SYSTEM" /F >nul 2>&1
 
-:: 删目录
-echo 删锟斤拷锟斤拷装目录 %INSTALL_DIR%
+:: ɾĿ¼
+echo ɾ����װĿ¼ %INSTALL_DIR%
 if exist "%INSTALL_DIR%" rmdir /s /q "%INSTALL_DIR%"
 
-:: 锟斤拷锟斤拷锟斤拷源锟斤拷锟斤拷锟斤拷
-echo 锟斤拷锟斤拷锟斤拷源锟斤拷锟斤拷锟斤拷锟斤拷刷锟斤拷锟揭硷拷锟剿碉拷
+:: ������Դ������
+echo ������Դ��������ˢ���Ҽ��˵�
 taskkill /IM explorer.exe /F >nul 2>&1
 start "" explorer.exe
 
-echo 卸锟斤拷锟斤拷伞锟�
+echo ж����ɡ�?
 pause
 exit /b 0
